838f365eac6d2dfa7bc600d5b039a7308d2955bd
Lucas Grecco <lucasgrecco@gmail.com>
2018-11-13 18:19:45 -0200

    Instalador da POC

--- scripts/install-poc.sh
+++ scripts/install-poc.sh
@@ -2,9 +2,61 @@
 
 echo Insira a senha do usuário root
 echo Permissões ao diretório atualizadas
-chmod 777 -Rf /web
+chmod 777 -Rf ../
+
+COMPOSER_DIR="$(which composer)"
+#CURL_DIR="$(which curl)"
+WGET_DIR="$(which wget)"
+PHP_DIR="$(which wget)"
+MYSQL_DIR="$(which wget)"
+
+if [ -z "$WGET_DIR" ]
+then
+    echo "Parece que o WGET não está instalado. Instale o WGET e tente novamente."
+    exit 1
+fi
+
+if [ -z "$PHP_DIR" ]
+then
+    echo "Parece que o PHP não está instalado. Instale o PHP e tente novamente."
+    exit 1
+fi
+
+if [ -z "$MYSQL_DIR" ]
+then
+    echo "Parece que o Mysql/MariaDB não está instalado. Instale o Mysql ou o MariaDB e tente novamente."
+    exit 1
+fi
+
+if [ -z "$COMPOSER_DIR" ]
+then
+    echo -e "Não encontrei o composer instalado. Vou instalar, perai!"
+    EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+    if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+    then
+        >&2 echo 'ERROR: Invalid installer signature'
+        rm composer-setup.php
+        exit 1
+    fi
+
+    php composer-setup.php #--quiet
+    RESULT=$?
+    rm composer-setup.php
+    sudo mv composer.phar /usr/local/bin/composer
+#    echo $RESULT
+    if [[ "$RESULT" -eq 0 ]]
+    then
+        echo "Algo deu errado! Tente instalar manualmente: https://getcomposer.org/doc/00-intro.md#installation-linux-unix-macos"
+        exit 1
+    fi
+fi
+
+#exit
 echo Copiando o settings padrão
-cp /home/grecco/projects/php/contenta/web/sites/default/settings.default.php /home/grecco/projects/php/contenta/web/sites/default/settings.local.php
+cp ../web/sites/default/settings.default.php ../web/sites/default/settings.local.php
 
 echo Insira as credenciais do seu DB.
 echo ""
@@ -16,9 +68,6 @@ read -p "Username: " uservar
 echo ""
 read -sp "Password: " passvar
 echo ""
-#echo $uservar
-#echo $passvar
-#echo $dbnamevar
 
 echo "Dropando banco com nome ${dbnamevar}"
 echo ""
